d298b26884b10b1b7355e6978fa36cc4de3d955a
AlexSekret <alexsekret3@gmail.com>
2024-08-06 13:47:57 +0300

    there are to few changes to write atomic commits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,8 @@ application {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.0-RC1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("info.picocli:picocli:4.7.6")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
+import hexlet.code.Differ;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import static hexlet.code.Differ.*;
+//import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-public class App {
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(generate());
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello Hexlet!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
+
