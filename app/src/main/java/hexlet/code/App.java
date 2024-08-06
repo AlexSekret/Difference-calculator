3c8d74f39c12cb3e03152df81fe6a5de209f8301
AlexSekret <alexsekret3@gmail.com>
2024-08-06 14:36:29 +0300

    rewrite App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,7 +3,7 @@ import hexlet.code.Differ;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import static hexlet.code.Differ.*;
-//import picocli.CommandLine.Option;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 @Command(
@@ -12,6 +12,15 @@ import picocli.CommandLine.Parameters;
         version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1 = "";
+
+    @Parameters(paramLabel = "filepath1", description = "path to second file")
+    private String filepath2 = "";
+
     @Override
     public void run() {
         System.out.println(generate());
