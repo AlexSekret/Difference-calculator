a882cf3a22443544eb1509dc1eeb760e5889eecb
AlexSekret <alexsekret3@gmail.com>
2024-08-06 21:46:46 +0300

    setup jackson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation ("info.picocli:picocli:4.7.6")
     annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.2")
+
 }
 
 tasks.test {
