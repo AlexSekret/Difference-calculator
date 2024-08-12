193a5bed281a741e66efb791e2f7e2ec74faf09f
AlexSekret <alexsekret3@gmail.com>
2024-08-12 16:02:33 +0300

    rewrite tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,34 +1,33 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class DifferTest {
-    static String relFilepath1;
-    static String relFilepath2;
-    static String expected;
-
-    @BeforeAll
-    public static void setUp() {
-        relFilepath1 = "src/test/resources/fixtures/file1.json";
-        relFilepath2 = "src/test/resources/fixtures/file2.json";
-        expected = """
-                {
-                  - follow: false
-                  - host: hexlet.io.app
-                  + host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
+    @Test
+    public void jsonStylishTest() throws Exception {
+        var filePath1 = "src/test/resources/fixtures/file1.json";
+        var filePath2 = "src/test/resources/fixtures/file2.json";
+        var expectedPath = Paths.get("src/test/resources/fixtures/expectedStylishJSON.txt")
+                .toAbsolutePath().normalize();
+        var expected = Files.readString(expectedPath);
+        assertEquals(expected,
+                Differ.generate(filePath1, filePath2));
     }
 
     @Test
-    public void generateRelativePathTest() throws Exception {
-        assertEquals(expected, Differ.generate(relFilepath1, relFilepath2));
+    public void yamlStylishTest() throws Exception {
+        var filePath1 = "src/test/resources/fixtures/file1.yml";
+        var filePath2 = "src/test/resources/fixtures/file2.yml";
+        var expectedPath = Paths.get("src/test/resources/fixtures/expectedStylishYAML.txt")
+                .toAbsolutePath().normalize();
+        var expected = Files.readString(expectedPath);
+        assertEquals(expected,
+                Differ.generate(filePath1, filePath2));
     }
 }
