557cae046a6023a4416bfe017b5590289bd58c20
AlexSekret <alexsekret3@gmail.com>
2024-08-30 14:47:18 +0300

    refactor Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
+
 
 public class Differ {
 
@@ -35,38 +37,27 @@ public class Differ {
         setOfKeys.addAll(secondData.keySet());
         List<Map<String, Object>> diff = new ArrayList<>();
         for (var s : setOfKeys) {
-            Object oldValue = firstData.get(s);
-            Object newValue = secondData.get(s);
-            String firstValue = String.valueOf(oldValue);
-            String secondValue = String.valueOf(newValue);
-            if (firstData.containsKey(s) && secondData.containsKey(s)) {
-                if (firstValue.equals(secondValue)) {
-                    Map<String, Object> mapDif = new LinkedHashMap<>();
-                    mapDif.put("key", s);
-                    mapDif.put("type", "not-changed");
-                    mapDif.put("value", oldValue);
-                    diff.add(mapDif);
-                } else {
-                    Map<String, Object> mapDif = new LinkedHashMap<>();
-                    mapDif.put("key", s);
-                    mapDif.put("type", "updated");
-                    mapDif.put("value1", oldValue);
-                    mapDif.put("value2", newValue);
-                    diff.add(mapDif);
-                }
-            } else if (firstData.containsKey(s) && !secondData.containsKey(s)) {
-                Map<String, Object> mapDif = new LinkedHashMap<>();
-                mapDif.put("key", s);
+            Object value1 = firstData.get(s);
+            Object value2 = secondData.get(s);
+            Map<String, Object> mapDif = new LinkedHashMap<>();
+            mapDif.put("key", s);
+            if (firstData.containsKey(s) && !secondData.containsKey(s)) {
                 mapDif.put("type", "removed");
-                mapDif.put("value", oldValue);
-                diff.add(mapDif);
+                mapDif.put("value", value1);
             } else if (!firstData.containsKey(s) && secondData.containsKey(s)) {
-                Map<String, Object> mapDif = new LinkedHashMap<>();
-                mapDif.put("key", s);
                 mapDif.put("type", "added");
-                mapDif.put("value", newValue);
-                diff.add(mapDif);
+                mapDif.put("value", value2);
+            } else {
+                if (Objects.equals(value1, value2)) {
+                    mapDif.put("type", "not-changed");
+                    mapDif.put("value", value1);
+                } else {
+                    mapDif.put("type", "updated");
+                    mapDif.put("value1", value1);
+                    mapDif.put("value2", value2);
+                }
             }
+            diff.add(mapDif);
         }
         return diff;
     }
