110bae6e9c40fe2efb39bd92af44d5500f4479da
AlexSekret <alexsekret3@gmail.com>
2024-09-04 20:33:45 +0300

    try to fix CodeClimate Cognitive Complexity issues with Stylish.json

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -5,23 +5,22 @@ import java.util.Map;
 
 public class Stylish implements DiffFormat {
     @Override
-    public final String getFormatedString(List<Map<String, Object>> diff) {
+    public final String getFormatedString(List<Map<String, Object>> diff) throws IllegalStateException {
         var result = new StringBuilder("{\n");
         for (var e : diff) {
-            var key = e.get("key");
             var type = e.get("type");
+            var key = e.get("key");
             var value = e.get("value");
             var value1 = e.get("value1");
             var value2 = e.get("value2");
-            if (type.equals("updated")) {
-                result.append(String.format("  - %s: %s%n", key, value1));
-                result.append(String.format("  + %s: %s%n", key, value2));
-            } else if (type.equals("added")) {
-                result.append(String.format("  + %s: %s%n", key, value));
-            } else if (type.equals("removed")) {
-                result.append(String.format("  - %s: %s%n", key, value));
-            } else if (type.equals("not-changed")) {
-                result.append(String.format("    %s: %s%n", key, value));
+            switch (type.toString()) {
+                case "updated" -> {
+                    result.append(String.format("  - %1$s: %2$s%n  + %1$s: %3$s%n", key, value1, value2));
+                }
+                case "added" -> result.append(String.format("  + %s: %s%n", key, value));
+                case "removed" -> result.append(String.format("  - %s: %s%n", key, value));
+                case "not-changed" -> result.append(String.format("    %s: %s%n", key, value));
+                default -> throw new IllegalStateException("Unexpected value: " + type);
             }
         }
         result.append("}");
